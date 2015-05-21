d86f763ed2580562bd78448b4db5044c7e933fa4
Juan Carlos Alonso Holmstron <j.carlos.alonso.h@gmail.com>
2015-05-21 16:41:36 -0300

    Final stub install

--- chef/installer/install.sh
+++ chef/installer/install.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 # Stub install.sh
-# TODO: make this smarter, though works
 
-sudo su - root
-echo "172.17.100.47 chef-cl.nisum.com" >> /etc/hosts
-sudo rpm -ivh /chef/installer/chef-12.3.0-1.el6.x86_64.rpm
-sudo mkdir -p /etc/chef/trusted_certs/; sudo rsync -avrPO /chef/trusted_certs/ /etc/chef/trusted_certs/
+sudo -u root echo "172.17.100.47 chef-cl.nisum.com" >> /etc/hosts
+sudo -u root rpm -ivh /chef/installer/chef-12.3.0-1.el6.x86_64.rpm
+sudo mkdir -p /etc/chef/trusted_certs/;
+sudo rsync -avrPO /chef/trusted_certs/ /etc/chef/trusted_certs/
